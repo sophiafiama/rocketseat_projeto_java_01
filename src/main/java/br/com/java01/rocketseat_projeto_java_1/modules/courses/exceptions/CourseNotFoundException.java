999bb65dcecb0a4b891833c6c56a8a64de0c5194
Tayna Alves Rodrigues <tayna.alves@mercadolivre.com>
2024-11-10 15:43:25 -0300

    feature: update courseNotFoundException to call NotFoundException

--- src/main/java/br/com/java01/rocketseat_projeto_java_1/modules/courses/exceptions/CourseNotFoundException.java
+++ src/main/java/br/com/java01/rocketseat_projeto_java_1/modules/courses/exceptions/CourseNotFoundException.java
@@ -1,4 +1,22 @@
 package br.com.java01.rocketseat_projeto_java_1.modules.courses.exceptions;
 
-public class CourseNotFoundException extends RuntimeException {
+import java.io.Serial;
+
+/**
+ * Exception thrown when a course with the specified ID is not found.
+ *
+ */
+public class CourseNotFoundException extends NotFoundException {
+    @Serial
+    private static final long serialVersionUID = 321422596L;
+    private static final String CODE = "Not found";
+    private static final String DESCRIPTION = "Curso com id %s não encontrado";
+
+    /**
+     *
+     * @param courseId the ID of the course that was not found
+     */
+    public CourseNotFoundException(Long courseId) {
+        super(CODE, String.format(DESCRIPTION, courseId));
+    }
 }
